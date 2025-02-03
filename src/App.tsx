eefcdcad105efd9109ee65dbc7e937da4b7237a5
BogdanF <sovtour@gmail.com>
2025-02-03 13:36:54 +0100

    lesson13

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,71 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import GlobalStyles from './styles/GlobalStyles'
+
+//lessons
+// import Lesson06 from './lessons/Lesson06/Lesson06'
+// import Lesson07 from './lessons/Lesson07/Lesson07'
+// import Lesson08 from './lessons/Lesson08/Lesson08'
+// import Lesson09 from './lessons/Lesson09/Lesson09'
+// import Lesson10 from './lessons/Lesson10/Lesson10'
+// import Lesson12 from 'lessons/Lesson12/Lesson12'
+//---Lesson 13
+import Layout from 'components/Layout/Layout'
+import Home from 'pages/Home/Home'
+import About from 'pages/About/About'
+import Users from 'pages/Users/Users'
+import User from 'pages/Users/components/User/User'
 
 
-import GlobalStyles from "styles/GlobalStyles";
-import Lesson11 from "./lesson/Lesson11/Lesson11";
-import Homework12 from "homeworks/Homework12/Homework12";
+//homeworks
+// import Homework08 from './homeworks/Homework08/Homework08'
+// import Homework09 from './homeworks/Homework09/Homework09'
+// import Homework10 from './homeworks/Homework10/Homework10'
+// import Homework12 from 'homeworks/Homework12/Homework12'
+
+//consultations
+// import Consultation03 from './consultations/Consultation03/Consultation03'
+// import Consultation_04 from './consultations/Consultation_04/Consultation_04'
 
 
 function App() {
-  return (
-    <>
 
+  return (
+    //BrowserRouter - компонент из react-router-dom, который позволяет нам работать с маршрутами
+    // по всему приложению
+    <BrowserRouter>
       <GlobalStyles />
-      {/* Topic: TypeScript - Introduction */}
+      <Layout>
+        {/* Routes - собирает все маршруты приложения */}
+        <Routes>
+          {/* Route - компонент, в который передаётся маршрут и контент, 
+         который нужно отрисовать по этому маршруту в Layout */}
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/users' element={<Users />} />
+          <Route path='/users/user' element={<User />} />
+        </Routes>
+      </Layout>
+      {/* --Topic: TypeScript - Introduction */}
       {/* <Lesson06/> */}
-      {/* <Consultation03/> */}
-      {/* Topic: TypeScript - Object types, enum */}
+      {/* <Consultation03 /> */}
+      {/* --Topic: TypeScript - Object types, enum */}
       {/* <Lesson07 /> */}
-      {/* Topic: Styling components */}
+      {/* --Topic: Styling components */}
       {/* <Lesson08 /> */}
       {/* <Homework08 /> */}
-      {/* Topic: Control components, useEffect */}
-     {/*  <Lesson09 /> */}
-			{/* <Consultation_04 /> */}
-     {/*  <Homework09 /> */}
-		 {/* <Lesson10 /> */}
-		 {/*<Feedback title="Feedback" initialLikes={0} initialDislikes={0} theme="light" onReaction={(type) => console.log('Reaction:', type)} />*/}
-		 <Homework12 />
-      {/* <Global styles={globalStyles} /> */}
-      {/* <Lesson11 /> */}
-			  {/* --Topic: formik, yup */}
-				{/* <Lesson11 /> */}
-				
-
-    </>
-  );
+      {/* --Topic: Control components, useEffect */}
+      {/* <Lesson09 /> */}
+      {/* <Consultation_04 /> */}
+      {/* <Homework09 /> */}
+      {/* --Topic: useEffect, axios */}
+      {/* <Lesson10 /> */}
+      {/* <Homework10 /> */}
+      {/* --Topic: formik, yup */}
+      {/* <Lesson12 /> */}
+      {/* <Homework12/> */}
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/Users/components/User/User.tsx
@@ -0,0 +1,16 @@
+
+
+import { UserInfo, UserPage, UserTitle } from "./styles";
+
+function User() {
+
+
+  return (
+    <UserPage>
+      <UserTitle>User Page</UserTitle>
+      <UserInfo>Здесь отображается информация о пользователе</UserInfo>
+    </UserPage>
+  )
+}
+
+export default User;
